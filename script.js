fc0842093f120d5104ec75ccb51081714f261740
Deepak Verma <Deepak Verma>
2024-06-23 19:09:02 +0530

    small_practice in javascipt

---
+++ script.js
@@ -0,0 +1,16 @@
+let input = document.getElementById("input");
+
+function onfun(){
+  input.style.backgroundColor = "red";
+}
+
+function offun(){
+  input.style.backgroundColor = "yellow";
+}
+
+function funsubmit(){
+  alert("You want to submit the form ?? ")
+}
+function onselectfun(){
+  alert("You select somethimg")
+}
